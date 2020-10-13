80d7646c75ab5fd39cbb6df9fc4468fbd805ff66
TristanB1994 <t.blandford.dev@gmail.com>
2020-10-13 00:30:42 -0400

    updated flask dependancy

--- testdriven-app/services/users/project/__init__.py
+++ testdriven-app/services/users/project/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify
 
+# instantiate app object
 app = Flask(__name__)
+
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
+
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
